86d7aede3470ed83b70dc017037421dd676a6eb1
cho <hisstoryxx@gmail.com>
2023-07-14 12:16:07 +0900

    water meshPhysicalMaterial setting

--- Experience/World/Room.js
+++ Experience/World/Room.js
@@ -23,6 +23,15 @@ export default class Room {
                     groupchild.receiveShadow = true;
                 })
             }
+
+            if(child.name === "water"){
+                child.material = new THREE.MeshPhysicalMaterial();
+                child.material.roughness = 0;
+                child.material.color.set(0xffffff);
+                child.material.ior = 3;
+                child.material.transmission = 1;
+                child.material.opacity = 1;
+            }
         });
 
         this.scene.add(this.acturalRoom);
