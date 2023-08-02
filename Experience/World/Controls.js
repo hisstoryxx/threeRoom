a7f0bc538278280262bcdb7f07e81b69d36a6ad1
cho <hisstoryxx@gmail.com>
2023-08-02 12:01:32 +0900

    asscroll install & apply

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -3,6 +3,7 @@ import * as THREE from "three"
 import { Camera, Scene } from "three";
 import GSAP from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import ASScroll from '@ashthornton/asscroll'
 
 export default class Controls {
     constructor(){
@@ -22,8 +23,51 @@ export default class Controls {
         GSAP.registerPlugin(ScrollTrigger);
  
         // this.setPath();
+        this.setSmoothScroll();
         this.setScrollTrigger();
 
+    }
+
+    setupASScroll() {
+        // https://github.com/ashthornton/asscroll
+        const asscroll = new ASScroll({
+          ease: 0.3,
+          disableRaf: true });
+      
+      
+        GSAP.ticker.add(asscroll.update);
+      
+        ScrollTrigger.defaults({
+          scroller: asscroll.containerElement });
+      
+      
+        ScrollTrigger.scrollerProxy(asscroll.containerElement, {
+          scrollTop(value) {
+            if (arguments.length) {
+              asscroll.currentPos = value;
+              return;
+            }
+            return asscroll.currentPos;
+          },
+          getBoundingClientRect() {
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+          },
+          fixedMarkers: true });
+      
+      
+        asscroll.on("update", ScrollTrigger.update);
+        ScrollTrigger.addEventListener("refresh", asscroll.resize);
+      
+        requestAnimationFrame(() => {
+          asscroll.enable({
+            newScrollElements: document.querySelectorAll(".gsap-marker-start, .gsap-marker-end, [asscroll]") });
+      
+        });
+        return asscroll;
+      }
+
+    setSmoothScroll(){
+        this.asscroll = this.setupASScroll();
     }
 
     setScrollTrigger(){
@@ -188,8 +232,67 @@ export default class Controls {
                     
             // all 
             "all": () => {
+                this.sections = document.querySelectorAll(".section");
+                this.sections.forEach(section => {
+                    this.progressWrapper = 
+                        section.querySelector(".progress-wrapper");
+                    this.progressBar = 
+                        section.querySelector(".progress-bar")
+                    if(section.classList.contains("right")){
+                        GSAP.to(section, {
+                            borderTopLeftRadius: 10,
+                            scrollTrigger: {
+                                trigger: section,
+                                start: "top bottom",
+                                end: "top top",
+                                scrub: 0.6,
+                            }
+                        })
+                        GSAP.to(section, {
+                            borderBottomLeftRadius: 700,
+                            scrollTrigger: {
+                                trigger: section,
+                                start: "bottom bottom",
+                                end: "bottom top",
+                                scrub: 0.6,
+                            }
+                        })
+                    }else {
+                        GSAP.to(section, {
+                            borderTopRightRadius: 10,
+                            scrollTrigger: {
+                                trigger: section,
+                                start: "top bottom",
+                                end: "top top",
+                                scrub: 0.6,
+                            }
+                        })
+                        GSAP.to(section, {
+                            borderBottomRightRadius: 700,
+                            scrollTrigger: {
+                                trigger: section,
+                                start: "bottom bottom",
+                                end: "bottom top",
+                                scrub: 0.6,
+                            }
+                        })
+                    }
+                    GSAP.from(this.progressBar, {
+                        scaleY: 0,
+                        scrollTrigger: {
+                            trigger: section,
+                            start: "top top",
+                            end: "bottom bottom",
+                            scrub: 0.4,
+                            pin: this.progressWrapper,
+                            pinSpacing: false,
+
+                        }
+                    })
+                })
+                
                 // Mini Platform Animations
-                console.log(this.room.children)
+                // console.log(this.room.children)
                 // Third-section ------------------------
                 this.secondPartTimeline = new GSAP.timeline({
                     scrollTrigger: {
